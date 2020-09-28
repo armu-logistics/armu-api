2e052fe4a059f38fb7c6438accb0afca2a0f48a2
anthony.baru <anthonybaru@gmail.com>
2020-09-28 17:26:21 +0300

    commented code that made app crash

--- app/routes/farmer.routes.js
+++ app/routes/farmer.routes.js
@@ -1,15 +1,15 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/farmer/farmer.controller");
 module.exports = function (app) {
-  app.post(
-    "/api/farmer/profile/create-farmer-details",
-    controller.createFarmerDetails
-  );
-  app.post(
-    "/api/farmer/profile/update-farmer-details",
-    [authJwt.verifyToken, authJwt.isFarmer],
-    controller.updateFarmerDetails
-  );
+  // app.post(
+  //   "/api/farmer/profile/create-farmer-details",
+  //   controller.createFarmerDetails
+  // );
+  // app.post(
+  //   "/api/farmer/profile/update-farmer-details",
+  //   [authJwt.verifyToken, authJwt.isFarmer],
+  //   controller.updateFarmerDetails
+  // );
   app.post(
     "/api/farmer/profile/create-farm",
     [authJwt.verifyToken, authJwt.isFarmer],
