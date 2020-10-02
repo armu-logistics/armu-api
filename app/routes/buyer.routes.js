d35d1470fb67859727f283de5df1c7f38f535532
anthony-baru <anthonybaru@gmail.com>
2020-10-02 15:53:02 +0300

    buyer route fix

--- app/routes/buyer.routes.js
+++ app/routes/buyer.routes.js
@@ -1,17 +1,3 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/buyer/buyer.controller");
-<<<<<<< HEAD
-module.exports = function (app) {
-  // app.post(
-  //   "/api/buyer/profile/create-buyer-details",
-  //   controller.createBuyerDetails
-  // );
-  // app.post(
-  //   "/api/buyer/profile/update-buyer-details",
-  //   [authJwt.verifyToken, authJwt.isBuyer],
-  //   controller.updateBuyerDetails
-  // );
-};
-=======
 module.exports = function (app) {};
->>>>>>> setup
