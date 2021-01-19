5f953930fa887f837515f41e7cfbd47a5be448cd
anthony-baru <anthonybaru@gmail.com>
2021-01-20 00:57:17 +0300

    added buyer get products api

--- app/controllers/buyer/buyer.controller.js
+++ app/controllers/buyer/buyer.controller.js
@@ -3,4 +3,48 @@ const Buyer = db.buyer;
 const User = db.user;
 const Joi = require("joi");
 const validate = require("../../util/validation");
+const Farmer = db.farmer;
+const Farm = db.farm;
+const FarmerProduct = db.farmerProduct;
+const ProductGrade = db.productGrade;
+const Product = db.product;
+const Grade = db.grade;
 let errHandler = new Error();
+
+exports.getPostedProducts = (req, res) => {
+  let farmerProductsFoundInfo;
+  console.log(db);
+  FarmerProduct.findAll({
+    include: [
+      { model: ProductGrade, include: [{ model: Product }, { model: Grade }] },
+      {
+        model: Farm,
+        required: true,
+        include: {
+          model: Farmer,
+          required: true,
+          include: {
+            model: User,
+            attributes: ["id", "name", "mobile", "email", "kra_pin"],
+            required: true,
+          },
+        },
+      },
+    ],
+  })
+    .then((farmerProductsFound) => {
+      farmerProductsFoundInfo = farmerProductsFound;
+      if (farmerProductsFoundInfo.length == 0) {
+        errHandler.message = ["No posted products found."];
+        errHandler.statusCode = 404;
+        throw errHandler;
+      }
+      res.send(farmerProductsFoundInfo);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res
+        .status(err.statusCode || 500)
+        .send({ success: false, message: err.message, data: err.data });
+    });
+};
--- app/routes/buyer.routes.js
+++ app/routes/buyer.routes.js
@@ -1,3 +1,9 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/buyer/buyer.controller");
-module.exports = function (app) {};
+module.exports = function (app) {
+  app.get(
+    "/api/buyer/get-posted-products",
+    [authJwt.verifyToken, authJwt.isBuyer],
+    controller.getPostedProducts
+  );
+};
