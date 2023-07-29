cc03bd70ffce085f504487b5b4ae7888ab280c59
Anthony Baru <anthonybaru@gmail.com>
2023-07-29 19:33:53 +0300

    returned routes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require("./app");
 const PORT = process.env.PORT || 8085;
-// const { sync } = require("./app/util/sync");
+const { sync } = require("./app/util/sync");
 
 // database
-// const db = require("./app/models");
+const db = require("./app/models");
 function serverListen(port) {
   // set port, listen for requests
   app.listen(port, () => {
@@ -13,8 +13,8 @@ function serverListen(port) {
 
 async function startApplication() {
   try {
-    // await db.sequelize.sync({ alter: true });
-    // await sync();
+    await db.sequelize.sync({ alter: true });
+    await sync();
     serverListen(PORT);
   } catch (error) {
     console.error(error);
